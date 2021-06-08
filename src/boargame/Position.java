eff24067405469085e8ff422e8d718f250d16d1f
MacLovin <monteiro9961@gmail.com>
2021-06-07 23:44:53 -0300

    Possible moves of a piece

--- src/boargame/Position.java
+++ src/boargame/Position.java
@@ -21,6 +21,11 @@ public class Position {
 		this.column = column;
 	}
 	
+	public void setValue(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
 	public String toString() {
 		return row + ", " + column;
 	}
